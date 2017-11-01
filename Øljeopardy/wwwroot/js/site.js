383e8e5a0c55873f34362c0efb11765f48051ded
Unknown <alexander@copalex.com>
2017-11-01 19:27:28 +0100

    Added option to swap AnswerQuestions

--- Øljeopardy/wwwroot/js/site.js
+++ Øljeopardy/wwwroot/js/site.js
@@ -11,3 +11,23 @@ function ValidateForm(form) {
     }
     form.submit();
 }
+
+function MoveAnswerQuestion(position, direction) {
+    var thisQuestionId = 'q'.concat(position);
+    var thisAnswerId = 'a'.concat(position);
+    var targetQuestionId = (direction === 'up') ? 'q'.concat(position - 1) : 'q'.concat(position + 1);
+    var targetAnswerId = (direction === 'up') ? 'a'.concat(position - 1) : 'a'.concat(position + 1);
+    var thisQuestion = document.getElementById(thisQuestionId);
+    var thisQuestionValue = thisQuestion.value;
+    var thisAnswer = document.getElementById(thisAnswerId);
+    var thisAnswerValue = thisAnswer.value;
+    var targetQuestion = document.getElementById(targetQuestionId);
+    var targetQuestionValue = targetQuestion.value;
+    var targetAnswer = document.getElementById(targetAnswerId);
+    var targetAnswerValue = targetAnswer.value;
+
+    thisQuestion.value = targetQuestionValue;
+    thisAnswer.value = targetAnswerValue;
+    targetQuestion.value = thisQuestionValue;
+    targetAnswer.value = thisAnswerValue;
+}
