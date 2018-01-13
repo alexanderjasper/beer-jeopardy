bbed6e5bb68a89adbaa9316924e41cbc67eb4bd6
Unknown <alexander@copalex.com>
2018-01-13 12:39:41 +0100

    Installed SignalR packages

---
+++ Øljeopardy/Services/UpdateCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Oljeopardy.Services
+{
+    public class UpdateCheck : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
